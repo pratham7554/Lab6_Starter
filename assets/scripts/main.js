9817d3fe8d25561e6d68f0c0d778ce1e3f9c37ef
PrathamMittal's MacBook VSCode <pmittal@ucsd.edu>
2022-11-07 14:56:00 -0800

    Expose check 1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,12 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  var recipeVar = localStorage.getItem('recipes');
+  var parsedRecipe = JSON.parse(recipeVar);
+  if (recipeVar == null) {
+    return [];
+  }
+  return parsedRecipe;
 }
 
 /**
@@ -35,10 +41,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  var mainElementRef = document.querySelector("main");
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  for (let i=0; i < recipes.length; i++) {
+    let currentRecipe = document.createElement('recipe-card');
+    currentRecipe.data = recipes[i];
+    mainElementRef.append(currentRecipe);
+  }
 }
 
 /**
