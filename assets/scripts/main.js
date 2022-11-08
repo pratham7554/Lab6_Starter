46ee7168b7bb2ec108ebc073bad6451725f7f3ba
PrathamMittal's MacBook VSCode <pmittal@ucsd.edu>
2022-11-07 17:00:29 -0800

    README updated

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -95,7 +95,8 @@ function initFormHandler() {
     // B7. TODO - Add the recipeObject data to <recipe-card> using element.data
     recipeCard.data = recipeObject;
     // B8. TODO - Append this new <recipe-card> to <main>
-    document.querySelector("main").append(recipeCard);
+    let mainVar = document.querySelector("main");
+    mainVar.append(recipeCard);
     // B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
     //            then save the recipes array back to localStorage
     var recipeArray = getRecipesFromStorage();
